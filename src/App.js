4e9eb4e9cda5d69839c393190eeffdb2063961db
Hector Rubi <hector.rubi@guros.com>
2021-10-20 14:49:08 -0500

    Add counter of todos

--- src/App.js
+++ src/App.js
@@ -7,21 +7,38 @@ import { TodoList } from "./TodoList";
 import { TodoItem } from "./TodoItem";
 import { CreateTodoButton } from "./CreateTodoButton";
 
-const todos = [
+const defaultTodos = [
   { text: 'Cortar cebolla', completed: true },
-  { text: 'Tomar el curso de intro a react', completed: false },
+  { text: 'Tomar el curso de intro a react', completed: true },
   { text: 'Llorar con la llorona', completed: false },
 ];
 
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(todo => (todo.completed)).length;
+  const totalTodos = todos.length;
+
   return (
     <React.Fragment>
-      <TodoCounter />
-      <TodoSearch />
+      <TodoCounter
+        total={totalTodos}
+        completed={completedTodos}
+      />
+
+      <TodoSearch
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
 
       <TodoList>
         {todos.map(todo => (
-          <TodoItem key={todo.text} text={todo.text} completed={todo.completed} />
+          <TodoItem 
+            key={todo.text} 
+            text={todo.text} 
+            completed={todo.completed} 
+          />
         ))}
       </TodoList>
 
