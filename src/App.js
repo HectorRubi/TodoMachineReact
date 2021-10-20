44376faef65f7dd60ee8a59e79c8739da2dd92b1
Hector Rubi <hector.rubi@guros.com>
2021-10-20 12:33:41 -0500

    Add css styles to components

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { TodoItem } from "./TodoItem";
 import { CreateTodoButton } from "./CreateTodoButton";
 
 const todos = [
-  { text: 'Cortar cebolla', completed: false },
+  { text: 'Cortar cebolla', completed: true },
   { text: 'Tomar el curso de intro a react', completed: false },
   { text: 'Llorar con la llorona', completed: false },
 ];
@@ -21,7 +21,7 @@ function App() {
 
       <TodoList>
         {todos.map(todo => (
-          <TodoItem key={todo.text} text={todo.text} />
+          <TodoItem key={todo.text} text={todo.text} completed={todo.completed} />
         ))}
       </TodoList>
 
