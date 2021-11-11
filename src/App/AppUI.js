63eda211d82bccbfef1351af5137ff43b488ab57
Hector Rubi <hector.rubi@guros.com>
2021-11-10 23:04:07 -0600

    Add form to add todo elements

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -5,6 +5,7 @@ import { TodoCounter } from "../TodoCounter";
 import { TodoSearch } from "../TodoSearch";
 import { TodoList } from "../TodoList";
 import { TodoItem } from "../TodoItem";
+import { TodoForm } from "../TodoForm";
 import { CreateTodoButton } from "../CreateTodoButton";
 import { Modal } from "../Modal";
 
@@ -42,7 +43,7 @@ function AppUI() {
 
       {!!openModal && (
         <Modal>
-          <p>Hola mundo</p>
+          <TodoForm />
         </Modal>
       )}
 
